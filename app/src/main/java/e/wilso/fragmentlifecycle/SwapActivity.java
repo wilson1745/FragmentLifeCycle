904f6ea8315c92c6476bc7b2eec2acc440e78a1f
wilson1745 <wilson-power0130@hotmail.com>
2018-06-01 13:23:48 +0100

    onKeyDown

--- app/src/main/java/e/wilso/fragmentlifecycle/SwapActivity.java
+++ app/src/main/java/e/wilso/fragmentlifecycle/SwapActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 
 public class SwapActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class SwapActivity extends AppCompatActivity {
    private FragmentManager fragManager;
    private ContactFragment contact;
    private DetailFragment detail;
+   private boolean change = true;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +38,32 @@ public class SwapActivity extends AppCompatActivity {
       fab.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View view) {
-            swap();
+            if(change) {
+               swap();
+               change = false;
+            }
          }
       });
    }
 
    private void swap() {
+      android.support.v4.app.FragmentTransaction trans = fragManager.beginTransaction();
+      trans.remove(contact);
+      trans.add(R.id.container, detail);
+      trans.addToBackStack(null);
+      trans.commit();
+   }
 
+   @Override
+   public boolean onKeyDown(int keyCode, KeyEvent event) {
+      if(keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
          android.support.v4.app.FragmentTransaction trans = fragManager.beginTransaction();
-         trans.replace(R.id.container, detail);
-         trans.addToBackStack(null);
+         trans.remove(detail);
+         trans.add(R.id.container, contact);
          trans.commit();
+         change = true;
+         return true;
+      }
+      return super.onKeyDown(keyCode, event);
    }
 }
